ba96aff5981e5dceeb033ca429a5d4c71e967f39
Thomas Woehlke <thomas.woehlke@rub.de>
2019-04-01 22:57:17 +0200

    setting up SpringBoot

--- src/main/java/org/woehlke/simpleworklist/config/LoginSuccessHandler.java
+++ src/main/java/org/woehlke/simpleworklist/config/LoginSuccessHandler.java
@@ -1,4 +1,4 @@
-package org.woehlke.simpleworklist.application;
+package org.woehlke.simpleworklist.config;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
--- src/main/java/org/woehlke/simpleworklist/config/WebSecurityConfig.java
+++ src/main/java/org/woehlke/simpleworklist/config/WebSecurityConfig.java
@@ -14,7 +14,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.woehlke.simpleworklist.application.LoginSuccessHandler;
 import org.woehlke.simpleworklist.services.UserAccountSecurityService;
 
 @Configuration
