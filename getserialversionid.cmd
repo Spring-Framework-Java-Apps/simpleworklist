8b00c7127690378fbe58cc95df7ada2326550738
Thomas Woehlke <thomas.woehlke@rub.de>
2019-04-09 19:17:27 +0200

    update bootstrap frontend

--- getserialversionid.cmd
+++ getserialversionid.cmd
@@ -15,34 +15,34 @@ set MY_CLASSPATH_APP=%JAVA_HOME%\jre\lib\rt.jar;%CD%\target\classes
 set CLASSPATH=%MY_CLASSPATH_APP%;%MY_CLASSPATH_DEPS%
 echo %CLASSPATH%
 cd target\classes
-serialver org.woehlke.simpleworklist.entities.entities.impl.AuditModel > ..\..\etc\serialversions.txt
-serialver org.woehlke.simpleworklist.entities.enumerations.Language >> ..\..\etc\serialversions.txt
-serialver org.woehlke.simpleworklist.entities.enumerations.TaskEnergy >> ..\..\etc\serialversions.txt
-serialver org.woehlke.simpleworklist.entities.enumerations.TaskState >> ..\..\etc\serialversions.txt
-serialver org.woehlke.simpleworklist.entities.enumerations.TaskStateView >> ..\..\etc\serialversions.txt
-serialver org.woehlke.simpleworklist.entities.enumerations.TaskTime >> ..\..\etc\serialversions.txt
-serialver org.woehlke.simpleworklist.entities.enumerations.UserPasswordRecoveryStatus >> ..\..\etc\serialversions.txt
-serialver org.woehlke.simpleworklist.entities.enumerations.UserRegistrationStatus >> ..\..\etc\serialversions.txt
-serialver org.woehlke.simpleworklist.entities.enumerations.UserRole >> ..\..\etc\serialversions.txt
-serialver org.woehlke.simpleworklist.entities.Context  >> ..\..\etc\serialversions.txt
-serialver org.woehlke.simpleworklist.entities.Project  >> ..\..\etc\serialversions.txt
-serialver org.woehlke.simpleworklist.entities.Task  >> ..\..\etc\serialversions.txt
-serialver org.woehlke.simpleworklist.entities.User2UserMessage  >> ..\..\etc\serialversions.txt
-serialver org.woehlke.simpleworklist.entities.UserAccount  >> ..\..\etc\serialversions.txt
-serialver org.woehlke.simpleworklist.entities.UserPasswordRecovery  >> ..\..\etc\serialversions.txt
-serialver org.woehlke.simpleworklist.entities.UserRegistration  >> ..\..\etc\serialversions.txt
-serialver org.woehlke.simpleworklist.model.BreadcrumbItem  >> ..\..\etc\serialversions.txt
-serialver org.woehlke.simpleworklist.model.Breadcrumb  >> ..\..\etc\serialversions.txt
-serialver org.woehlke.simpleworklist.model.LoginForm >> ..\..\etc\serialversions.txt
-serialver org.woehlke.simpleworklist.model.NewContextForm >> ..\..\etc\serialversions.txt
-serialver org.woehlke.simpleworklist.model.UserRegistrationForm >> ..\..\etc\serialversions.txt
-serialver org.woehlke.simpleworklist.model.SearchResult >> ..\..\etc\serialversions.txt
-serialver org.woehlke.simpleworklist.model.UserAccountForm  >> ..\..\etc\serialversions.txt
-serialver org.woehlke.simpleworklist.model.UserChangeDefaultContextForm >> ..\..\etc\serialversions.txt
-serialver org.woehlke.simpleworklist.model.UserChangeLanguageForm  >> ..\..\etc\serialversions.txt
-serialver org.woehlke.simpleworklist.model.UserChangeNameForm >> ..\..\etc\serialversions.txt
-serialver org.woehlke.simpleworklist.model.UserChangePasswordForm >> ..\..\etc\serialversions.txt
-serialver org.woehlke.simpleworklist.model.UserSessionBean >> ..\..\etc\serialversions.txt
-@rem serialver org.woehlke.simpleworklist.model.UserDetailsBean >> ..\..\etc\serialversions.txt
+serialver org.woehlke.simpleworklist.oodm.enumerations.Language > ..\..\etc\serialversions.txt
+serialver org.woehlke.simpleworklist.oodm.enumerations.TaskEnergy >> ..\..\etc\serialversions.txt
+serialver org.woehlke.simpleworklist.oodm.enumerations.TaskState >> ..\..\etc\serialversions.txt
+serialver org.woehlke.simpleworklist.oodm.enumerations.TaskTime >> ..\..\etc\serialversions.txt
+serialver org.woehlke.simpleworklist.oodm.enumerations.UserPasswordRecoveryStatus >> ..\..\etc\serialversions.txt
+serialver org.woehlke.simpleworklist.oodm.enumerations.UserRegistrationStatus >> ..\..\etc\serialversions.txt
+serialver org.woehlke.simpleworklist.oodm.enumerations.UserRole >> ..\..\etc\serialversions.txt
+serialver org.woehlke.simpleworklist.oodm.entities.impl.AuditModel >> ..\..\etc\serialversions.txt
+serialver org.woehlke.simpleworklist.oodm.entities.Context  >> ..\..\etc\serialversions.txt
+serialver org.woehlke.simpleworklist.oodm.entities.Project  >> ..\..\etc\serialversions.txt
+serialver org.woehlke.simpleworklist.oodm.entities.Task  >> ..\..\etc\serialversions.txt
+serialver org.woehlke.simpleworklist.oodm.entities.User2UserMessage  >> ..\..\etc\serialversions.txt
+serialver org.woehlke.simpleworklist.oodm.entities.UserAccount  >> ..\..\etc\serialversions.txt
+serialver org.woehlke.simpleworklist.oodm.entities.UserPasswordRecovery  >> ..\..\etc\serialversions.txt
+serialver org.woehlke.simpleworklist.oodm.entities.UserRegistration  >> ..\..\etc\serialversions.txt
+serialver org.woehlke.simpleworklist.model.beans.BreadcrumbItem  >> ..\..\etc\serialversions.txt
+serialver org.woehlke.simpleworklist.model.beans.Breadcrumb  >> ..\..\etc\serialversions.txt
+serialver org.woehlke.simpleworklist.model.beans.LoginForm >> ..\..\etc\serialversions.txt
+serialver org.woehlke.simpleworklist.model.beans.NewContextForm >> ..\..\etc\serialversions.txt
+serialver org.woehlke.simpleworklist.model.beans.NewUser2UserMessage >> ..\..\etc\serialversions.txt
+serialver org.woehlke.simpleworklist.model.beans.SearchResult >> ..\..\etc\serialversions.txt
+serialver org.woehlke.simpleworklist.model.beans.UserAccountForm  >> ..\..\etc\serialversions.txt
+serialver org.woehlke.simpleworklist.model.beans.UserChangeDefaultContextForm >> ..\..\etc\serialversions.txt
+serialver org.woehlke.simpleworklist.model.beans.UserChangeLanguageForm  >> ..\..\etc\serialversions.txt
+serialver org.woehlke.simpleworklist.model.beans.UserChangeNameForm >> ..\..\etc\serialversions.txt
+serialver org.woehlke.simpleworklist.model.beans.UserChangePasswordForm >> ..\..\etc\serialversions.txt
+@rem serialver org.woehlke.simpleworklist.model.beans.UserDetailsBean >> ..\..\etc\serialversions.txt
+serialver org.woehlke.simpleworklist.model.beans.UserRegistrationForm >> ..\..\etc\serialversions.txt
+serialver org.woehlke.simpleworklist.model.beans.UserSessionBean >> ..\..\etc\serialversions.txt
 cd ..\..
 echo FINISHED
