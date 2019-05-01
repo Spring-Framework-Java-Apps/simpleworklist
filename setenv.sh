d726e923577c7efa57e24eaec3a610d4592fff60
Thomas Woehlke <thomas.woehlke@rub.de>
2019-05-02 00:28:44 +0200

    heroku setup

--- setenv.sh
+++
@@ -1,7 +0,0 @@
-export PORT=5000
-export JAVA_OPTS='-Xmx300m -Xss512k -XX:CICompilerCount=2'
-export MAIL_HOST=sslout.df.eu
-export MAIL_PORT=465
-export MAIL_USERNAME=news@thomas-woehlke.de
-export MAIL_PASSWORD=gecgy.gKC8u3
-export MAIL_FROM=news@thomas-woehlke.de
