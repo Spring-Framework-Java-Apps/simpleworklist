b80bc3b6f1eff97c5e5d96df77583d4c61736849
thomaswoehlke <thomas.woehlke@rub.de>
2020-03-31 14:21:18 +0200

    Fixed #78, Fixed #77

---
+++ getserialversionid.sh
@@ -0,0 +1,60 @@
+#!/usr/bin/env bash
+
+export PROJECT_PATH=`pwd`
+
+echo STARTED
+echo "CLASSPATH $CLASSPATH"
+echo "JAVA_HOME $JAVA_HOME"
+echo "HOME $HOME"
+echo "PROJECT_PATH $PROJECT_PATH"
+export CLASSPATH_FILE=$PROJECT_PATH/etc/classpath.txt
+export CLASSPATH=
+#@rem call mvnw.cmd --batch-mode --log-file=etc/mavenlog1.txt clean install -Pdeveloping -DskipTests=true -Dmaven.javadoc.skip=true -Dmdep.outputFile=etc\classpath.txt
+echo "START MAVEN"
+./mvnw --batch-mode --log-file=etc/mavenlog1.txt clean install -DskipTests=true -Dmaven.javadoc.skip=true
+#@rem call mvnw.cmd --batch-mode --log-file=etc/mavenlog2.txt dependency:unpack-dependencies
+./mvnw --batch-mode --log-file=etc/mavenlog3.txt dependency:build-classpath -Dmdep.outputFile=$CLASSPATH_FILE
+echo "FINISHED MAVEN"
+
+export MY_CLASSPATH_DEPS=`cat $CLASSPATH_FILE`
+export CLASSES_PATH=$PROJECT_PATH/target/classes
+export JAVA_RUNTIME_JAR_PATH=$JAVA_HOME/jre/lib/rt.jar
+
+export MY_CLASSPATH=$JAVA_RUNTIME_JAR_PATH:$CLASSES_PATH:$MY_CLASSPATH_DEPS
+export CLASSPATH=MY_CLASSPATH
+
+export SERIALVERSIONS_FILE="$PROJECT_PATH/etc/serialversions.txt"
+
+echo "CLASSPATH $CLASSPATH"
+cd $CLASSES_PATH
+serialver org.woehlke.simpleworklist.oodm.enumerations.Language > $SERIALVERSIONS_FILE
+serialver org.woehlke.simpleworklist.oodm.enumerations.TaskEnergy >> $SERIALVERSIONS_FILE
+serialver org.woehlke.simpleworklist.oodm.enumerations.TaskState >> $SERIALVERSIONS_FILE
+serialver org.woehlke.simpleworklist.oodm.enumerations.TaskTime >> $SERIALVERSIONS_FILE
+serialver org.woehlke.simpleworklist.oodm.enumerations.UserPasswordRecoveryStatus >> $SERIALVERSIONS_FILE
+serialver org.woehlke.simpleworklist.oodm.enumerations.UserRegistrationStatus >> $SERIALVERSIONS_FILE
+serialver org.woehlke.simpleworklist.oodm.enumerations.UserRole >> $SERIALVERSIONS_FILE
+serialver org.woehlke.simpleworklist.oodm.entities.impl.AuditModel >> $SERIALVERSIONS_FILE
+serialver org.woehlke.simpleworklist.oodm.entities.Context  >> $SERIALVERSIONS_FILE
+serialver org.woehlke.simpleworklist.oodm.entities.Project  >> $SERIALVERSIONS_FILE
+serialver org.woehlke.simpleworklist.oodm.entities.Task  >> $SERIALVERSIONS_FILE
+serialver org.woehlke.simpleworklist.oodm.entities.User2UserMessage  >> $SERIALVERSIONS_FILE
+serialver org.woehlke.simpleworklist.oodm.entities.UserAccount  >> $SERIALVERSIONS_FILE
+serialver org.woehlke.simpleworklist.oodm.entities.UserPasswordRecovery  >> $SERIALVERSIONS_FILE
+serialver org.woehlke.simpleworklist.oodm.entities.UserRegistration  >> $SERIALVERSIONS_FILE
+serialver org.woehlke.simpleworklist.model.beans.BreadcrumbItem  >> $SERIALVERSIONS_FILE
+serialver org.woehlke.simpleworklist.model.beans.Breadcrumb  >> $SERIALVERSIONS_FILE
+serialver org.woehlke.simpleworklist.model.beans.LoginForm >> $SERIALVERSIONS_FILE
+serialver org.woehlke.simpleworklist.model.beans.NewContextForm >> $SERIALVERSIONS_FILE
+serialver org.woehlke.simpleworklist.model.beans.NewUser2UserMessage >> $SERIALVERSIONS_FILE
+serialver org.woehlke.simpleworklist.model.beans.SearchResult >> $SERIALVERSIONS_FILE
+serialver org.woehlke.simpleworklist.model.beans.UserAccountForm  >> $SERIALVERSIONS_FILE
+serialver org.woehlke.simpleworklist.model.beans.UserChangeDefaultContextForm >> $SERIALVERSIONS_FILE
+serialver org.woehlke.simpleworklist.model.beans.UserChangeLanguageForm  >> $SERIALVERSIONS_FILE
+serialver org.woehlke.simpleworklist.model.beans.UserChangeNameForm >> $SERIALVERSIONS_FILE
+serialver org.woehlke.simpleworklist.model.beans.UserChangePasswordForm >> $SERIALVERSIONS_FILE
+#serialver org.woehlke.simpleworklist.model.beans.UserDetailsBean >> $SERIALVERSIONS_FILE
+serialver org.woehlke.simpleworklist.model.beans.UserRegistrationForm >> $SERIALVERSIONS_FILE
+serialver org.woehlke.simpleworklist.model.beans.UserSessionBean >> $SERIALVERSIONS_FILE
+cd $PROJECT_PATH
+echo FINISHED
---
+++ setenv-sample.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+export MAIL_SMTP_HOST=mailhost
+export MAIL_SMTP_PORT=465
+export MAIL_SMTP_USERNAME='mailuser'
+export MAIL_SMTP_PASSWORD='mailuserpassword'
+export MAIL_FROM='noreply@example.org'
+export MAIL_URL_APP_HOST='localhost:8080'
