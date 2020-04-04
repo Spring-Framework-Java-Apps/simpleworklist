18905d699079bc05cd38cbcc7c40bb7b56bb6bee
thomaswoehlke <thomas.woehlke@rub.de>
2020-04-04 09:11:09 +0200

    bugfixing

--- run.sh
+++ run.sh
@@ -2,5 +2,26 @@
 
 source setenv.sh
 
-./mvnw
+function setupHeroku() {
+    heroku login
+    heroku ps
+}
 
+function runHerokuLocal() {
+    heroku ps
+    .mvnw clean install
+    heroku local web
+    heroku open
+}
+
+function runDev() {
+    .mvnw
+}
+
+function main() {
+    runDev
+    #setupHeroku
+    #runHerokuLocal
+}
+
+main
