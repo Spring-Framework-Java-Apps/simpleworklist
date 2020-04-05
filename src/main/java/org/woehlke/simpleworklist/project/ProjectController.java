8f9b077fe2195a74b661cee29a6d29f2ea519ed0
thomaswoehlke <thomas.woehlke@rub.de>
2020-04-05 23:34:06 +0200

    URL PATH of Actions - Controller Methods Refactoring

--- src/main/java/org/woehlke/simpleworklist/project/ProjectController.java
+++ src/main/java/org/woehlke/simpleworklist/project/ProjectController.java
@@ -12,6 +12,10 @@ import org.springframework.web.bind.annotation.*;
 import org.woehlke.simpleworklist.common.AbstractController;
 import org.woehlke.simpleworklist.context.Context;
 import org.woehlke.simpleworklist.task.Task;
+import org.woehlke.simpleworklist.task.TaskEnergy;
+import org.woehlke.simpleworklist.task.TaskTime;
+import org.woehlke.simpleworklist.taskstate.TaskMoveService;
+import org.woehlke.simpleworklist.taskstate.TaskState;
 import org.woehlke.simpleworklist.user.account.UserAccount;
 import org.woehlke.simpleworklist.breadcrumb.Breadcrumb;
 import org.woehlke.simpleworklist.user.UserSessionBean;
@@ -30,10 +34,12 @@ import java.util.Locale;
 public class ProjectController extends AbstractController {
 
     private final ProjectControllerService projectControllerService;
+    private final TaskMoveService taskMoveService;
 
     @Autowired
-    public ProjectController(ProjectControllerService projectControllerService) {
+    public ProjectController(ProjectControllerService projectControllerService, TaskMoveService taskMoveService) {
         this.projectControllerService = projectControllerService;
+        this.taskMoveService = taskMoveService;
     }
 
     @RequestMapping(path = "/{projectId}", method = RequestMethod.GET)
@@ -227,6 +233,110 @@ public class ProjectController extends AbstractController {
             return "redirect:/project/root";
         } else {
             return "redirect:/project/" + newProjectId;
+        }
+    }
+
+    @RequestMapping(path = "/{projectId}/task/{sourceTaskId}/changeorderto/{destinationTaskId}", method = RequestMethod.GET)
+    public String changeTaskOrderIdWithinAProject(
+        @PathVariable("projectId") Project thisProject,
+        @PathVariable("sourceTaskId") Task sourceTask,
+        @PathVariable("destinationTaskId") Task destinationTask,
+        @ModelAttribute("userSession") UserSessionBean userSession,
+        Model model
+    ){
+        if(!sourceTask.isInRootProject()){
+            userSession.setLastProjectId(sourceTask.getProject().getId());
+        }
+        model.addAttribute("userSession",userSession);
+        log.info("-------------------------------------------------");
+        log.info("  changeTaskOrderIdWithinAProject");
+        log.info("-------------------------------------------------");
+        log.info("  source Task:      "+sourceTask.toString());
+        log.info("-------------------------------------------------");
+        log.info("  destination Task: "+destinationTask.toString());
+        log.info("-------------------------------------------------");
+        String returnUrl = "redirect:/taskstate/inbox";
+        boolean rootProject = sourceTask.isInRootProject();
+        returnUrl = "redirect:/project/0";
+        if(rootProject){
+            taskMoveService.moveOrderIdRootProject(sourceTask, destinationTask);
+        } else {
+            taskMoveService.moveOrderIdProject(sourceTask, destinationTask);
+            log.info("  DONE: taskMoveService.moveOrderIdProject (2)");
+            returnUrl = "redirect:/project/" + sourceTask.getProject().getId();
+        }
+        log.info("-------------------------------------------------");
+        return returnUrl;
+    }
+
+    @RequestMapping(path = "/{projectId}/task/add/", method = RequestMethod.GET)
+    public final String addNewTaskToProjectGet(
+        @ModelAttribute("userSession") UserSessionBean userSession,
+        Locale locale, Model model
+    ) {
+        UserAccount userAccount = userAccountLoginSuccessService.retrieveCurrentUser();
+        Task task = new Task();
+        task.setTaskState(TaskState.INBOX);
+        task.setTaskEnergy(TaskEnergy.NONE);
+        task.setTaskTime(TaskTime.NONE);
+        Boolean mustChooseArea = false;
+        if(userSession.getContextId() == 0L){
+            mustChooseArea = true;
+            task.setContext(userAccount.getDefaultContext());
+        } else {
+            Context context = contextService.findByIdAndUserAccount(userSession.getContextId(), userAccount);
+            task.setContext(context);
+        }
+        Breadcrumb breadcrumb = breadcrumbService.getBreadcrumbForShowRootProject(locale);
+        model.addAttribute("breadcrumb", breadcrumb);
+        model.addAttribute("mustChooseArea", mustChooseArea);
+        model.addAttribute("thisProjectId", 0L);
+        model.addAttribute("breadcrumb", breadcrumb);
+        model.addAttribute("task", task);
+        return "id/task/add";
+    }
+
+    @RequestMapping(path = "/{projectId}/task/add/", method = RequestMethod.POST)
+    public final String addNewTaskToProjectPost(
+        @PathVariable long projectId,
+        @ModelAttribute("userSession") UserSessionBean userSession,
+        @Valid Task task,
+        BindingResult result, Locale locale, Model model) {
+        Context context = super.getContext(userSession);
+        UserAccount userAccount = context.getUserAccount();
+        if (result.hasErrors()) {
+            for (ObjectError e : result.getAllErrors()) {
+                log.info(e.toString());
+            }
+            Project thisProject = projectControllerService.getProject(projectId, userAccount, userSession);
+            Boolean mustChooseArea = false;
+            task.setProject(thisProject);
+            task.setContext(thisProject.getContext());
+            Breadcrumb breadcrumb = breadcrumbService.getBreadcrumbForShowOneProject(thisProject,locale);
+            model.addAttribute("mustChooseArea", mustChooseArea);
+            model.addAttribute("thisProject", thisProject);
+            model.addAttribute("breadcrumb", breadcrumb);
+            model.addAttribute("task", task);
+            //return "task/addToProject";
+            return "id/task/add";
+        } else {
+            Project thisProject = projectService.findByProjectId(projectId);
+            task.setProject(thisProject);
+            task.setContext(thisProject.getContext());
+            if(task.getDueDate()==null){
+                task.setTaskState(TaskState.INBOX);
+            } else {
+                task.setTaskState(TaskState.SCHEDULED);
+            }
+            task.setFocus(false);
+            task.setContext(context);
+            long maxOrderIdProject = taskMoveService.getMaxOrderIdProject(task.getProject(),context);
+            task.setOrderIdProject(++maxOrderIdProject);
+            long maxOrderIdTaskState = taskMoveService.getMaxOrderIdTaskState(task.getTaskState(),task.getContext());
+            task.setOrderIdTaskState(++maxOrderIdTaskState);
+            task = taskService.saveAndFlush(task);
+            log.info(task.toString());
+            return "redirect:/project/" + projectId + "/";
         }
     }
 
