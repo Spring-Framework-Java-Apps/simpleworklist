c7b0015eefbcf51856acc9613e0d99ae054e6a82
thomaswoehlke <thomas.woehlke@rub.de>
2020-04-06 03:35:03 +0200

    URL PATH of Actions - Controller Methods Refactoring

--- src/main/java/org/woehlke/simpleworklist/common/ComparableById.java
+++ src/main/java/org/woehlke/simpleworklist/common/ComparableById.java
@@ -1,7 +1,5 @@
 package org.woehlke.simpleworklist.common;
 
-import org.woehlke.simpleworklist.common.AuditModel;
-
 public interface ComparableById<T extends AuditModel> {
     boolean equalsById(T otherObject);
     boolean equalsByUniqueConstraint(T otherObject);
