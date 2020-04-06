e353525efe4cea2bdf70f0a89218e136c3d129e8
thomaswoehlke <thomas.woehlke@rub.de>
2020-04-06 13:03:30 +0200

    URL PATH of Actions - Controller Methods Refactoring

--- src/test/java/org/woehlke/simpleworklist/userstories/F001ServerStartsTest.java
+++ src/test/java/org/woehlke/simpleworklist/userstories/F001ServerStartsTest.java
@@ -1,4 +1,19 @@
 package org.woehlke.simpleworklist.userstories;
 
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+
+@Slf4j
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class F001ServerStartsTest {
+
+    @LocalServerPort
+    int randomServerPort;
+
+    @Test
+    public void testStartup(){
+        log.info("testStartup");
+    }
 }
