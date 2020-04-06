c54fd5c4b993bb156c51025f3ca8cd90191fcb58
thomaswoehlke <thomas.woehlke@rub.de>
2020-04-06 03:08:01 +0200

    URL PATH of Actions - Controller Methods Refactoring

--- src/main/java/org/woehlke/simpleworklist/taskstate/TaskStateMoveController.java
+++ src/main/java/org/woehlke/simpleworklist/taskstate/TaskStateMoveController.java
@@ -3,15 +3,24 @@ package org.woehlke.simpleworklist.taskstate;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.*;
+import org.woehlke.simpleworklist.breadcrumb.Breadcrumb;
 import org.woehlke.simpleworklist.common.AbstractController;
 import org.woehlke.simpleworklist.context.Context;
 import org.woehlke.simpleworklist.project.Project;
 import org.woehlke.simpleworklist.task.Task;
 import org.woehlke.simpleworklist.task.TaskControllerService;
+import org.woehlke.simpleworklist.task.TaskEnergy;
+import org.woehlke.simpleworklist.task.TaskTime;
 import org.woehlke.simpleworklist.user.UserSessionBean;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.woehlke.simpleworklist.user.account.UserAccount;
+
+import javax.validation.Valid;
+import java.util.Locale;
 
 /**
  * Created by tw on 21.02.16.
@@ -33,6 +42,73 @@ public class TaskStateMoveController extends AbstractController {
         this.taskControllerService = taskControllerService;
     }
 
+    @RequestMapping(path = "/add", method = RequestMethod.GET)
+    public final String addNewTaskToInboxGet(
+        @ModelAttribute("userSession") UserSessionBean userSession,
+        Locale locale, Model model
+    ) {
+        log.info("addNewTaskToInboxGet");
+        UserAccount userAccount = userAccountLoginSuccessService.retrieveCurrentUser();
+        Task task = new Task();
+        task.setTaskState(TaskState.INBOX);
+        task.setTaskEnergy(TaskEnergy.NONE);
+        task.setTaskTime(TaskTime.NONE);
+        Boolean mustChooseContext = false;
+        if(userSession.getContextId() == 0L){
+            mustChooseContext = true;
+            task.setContext(userAccount.getDefaultContext());
+        } else {
+            Context context = contextService.findByIdAndUserAccount(userSession.getContextId(), userAccount);
+            task.setContext(context);
+        }
+        Breadcrumb breadcrumb = breadcrumbService.getBreadcrumbForTaskstate(TaskState.INBOX,locale);
+        model.addAttribute("breadcrumb", breadcrumb);
+        model.addAttribute("mustChooseArea", mustChooseContext);
+        model.addAttribute("breadcrumb", breadcrumb);
+        model.addAttribute("task", task);
+        return "taskstate/task/add";
+    }
+
+    @RequestMapping(path = "/add",  method = RequestMethod.POST)
+    public final String addNewTaskToInboxPost(
+        @ModelAttribute("userSession") UserSessionBean userSession,
+        @Valid Task task,
+        BindingResult result,
+        Locale locale,
+        Model model
+    ) {
+        log.info("addNewTaskToInboxPost");
+        Context context = super.getContext(userSession);
+        if (result.hasErrors()) {
+            for (ObjectError e : result.getAllErrors()) {
+                log.info(e.toString());
+            }
+            Boolean mustChooseArea = false;
+            task.setContext(context);
+            Breadcrumb breadcrumb = breadcrumbService.getBreadcrumbForTaskstate(TaskState.INBOX,locale);
+            model.addAttribute("mustChooseArea", mustChooseArea);
+            model.addAttribute("breadcrumb", breadcrumb);
+            model.addAttribute("task", task);
+            return "taskstate/task/add";
+        } else {
+            task.setProject(null);
+            if(task.getDueDate()==null){
+                task.setTaskState(TaskState.INBOX);
+            } else {
+                task.setTaskState(TaskState.SCHEDULED);
+            }
+            task.setFocus(false);
+            task.setContext(context);
+            long maxOrderIdProject = taskMoveService.getMaxOrderIdProject(task.getProject(),context);
+            task.setOrderIdProject(++maxOrderIdProject);
+            long maxOrderIdTaskState = taskMoveService.getMaxOrderIdTaskState(task.getTaskState(),task.getContext());
+            task.setOrderIdTaskState(++maxOrderIdTaskState);
+            task = taskService.saveAndFlush(task);
+            log.info(task.toString());
+            return "redirect:/taskstate/" + task.getTaskState().name().toLowerCase();
+        }
+    }
+
     @RequestMapping(path = "/{sourceTaskId}/changeorderto/{destinationTaskId}", method = RequestMethod.GET)
     public String changeTaskOrderId(
         @PathVariable("sourceTaskId") Task sourceTask,
@@ -52,8 +128,10 @@ public class TaskStateMoveController extends AbstractController {
     }
 
     @RequestMapping(path = "/{taskId}/move/to/project/{projectId}", method = RequestMethod.GET)
-    public final String moveTaskToAnotherProject(@PathVariable("taskId") Task task,
-                                                 @PathVariable long projectId) {
+    public final String moveTaskToAnotherProject(
+        @PathVariable("taskId") Task task,
+        @PathVariable long projectId
+    ) {
         if(projectId == 0) {
             task = taskMoveService.moveTaskToRootProject(task);
         } else {
@@ -91,7 +169,7 @@ public class TaskStateMoveController extends AbstractController {
         return "redirect:/taskstate/waiting";
     }
 
-    @RequestMapping(path = "/{taskId}/move/to/taskstate//someday", method = RequestMethod.GET)
+    @RequestMapping(path = "/{taskId}/move/to/taskstate/someday", method = RequestMethod.GET)
     public final String moveTaskToSomeday(@PathVariable("taskId") Task task) {
         log.info("dragged and dropped "+task.getId()+" to someday");
         task = taskMoveService.moveTaskToSomeday(task);
@@ -112,14 +190,14 @@ public class TaskStateMoveController extends AbstractController {
         return "redirect:/taskstate/completed";
     }
 
-    @RequestMapping(path = "/{taskId}/move/to/taskstate/trash", method = RequestMethod.GET)
+    @RequestMapping(path = "/{taskId}/move/to/trash", method = RequestMethod.GET)
     public final String moveTaskToTrash(@PathVariable("taskId") Task task) {
         log.info("dragged and dropped "+task.getId()+" to trash");
         task = taskMoveService.moveTaskToTrash(task);
         return "redirect:/taskstate/trash";
     }
 
-    @RequestMapping(path = "/completed/move/to/taskstate/trash", method = RequestMethod.GET)
+    @RequestMapping(path = "/completed/move/to/trash", method = RequestMethod.GET)
     public final String deleteallCompleted(
         @ModelAttribute("userSession") UserSessionBean userSession
     ) {
