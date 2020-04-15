b15de7d1e5b6b923f0ff8640a50af3939b0a3280
thomaswoehlke <thomas.woehlke@rub.de>
2020-04-15 21:51:21 +0200

    Maven Site and Documentation

--- src/main/java/org/woehlke/simpleworklist/application/testdata/TestDataController.java
+++ src/main/java/org/woehlke/simpleworklist/application/testdata/TestDataController.java
@@ -1,4 +1,4 @@
-package org.woehlke.simpleworklist.domain.testdata;
+package org.woehlke.simpleworklist.application.testdata;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
