7ed7fac10437b8a9c56a1940dd0c080c47d770c0
thomaswoehlke <thomas.woehlke@gmail.com>
2020-08-26 20:16:04 +0200

    changed build.sh

--- build.sh
+++ build.sh
@@ -21,13 +21,13 @@ function firstSetup() {
 function setupTravis() {
     export JAVA_OPTS=$JAVA_OPTS_RUN_DEFAULT
     showSettings
-    ./mvnw -e -DskipTests=true -B -V install -Dmaven.javadoc.skip=true
-    ./mvnw -e -DskipTests=true -B -V dependency:purge-local-repository
-    ./mvnw -e -DskipTests=true -B -V clean
-    ./mvnw -e -DskipTests=true -B -V dependency:resolve dependency:resolve-plugins dependency:sources
-    ./mvnw -e -DskipTests=true -B -V dependency:tree
-    ./mvnw -e -DskipTests=true -B -V clean package spring-boot:repackage
-    #./mvnw -e -DskipTests=true -B -V site
+    ./mvnw -e -DskipTests=true -B -V install -Dmaven.javadoc.skip=true && \
+    ./mvnw -e -DskipTests=true -B -V dependency:purge-local-repository && \
+    ./mvnw -e -DskipTests=true -B -V clean && \
+    ./mvnw -e -DskipTests=true -B -V dependency:resolve dependency:resolve-plugins dependency:sources && \
+    ./mvnw -e -DskipTests=true -B -V dependency:tree && \
+    ./mvnw -e -DskipTests=true -B -V clean package spring-boot:repackage && \
+    ./mvnw -e -DskipTests=true -B -V site
 }
 
 function main() {
