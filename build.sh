1c80be3d0ba20153051e8d87163326f1bcd0bb0a
thomaswoehlke <thomas.woehlke@gmail.com>
2020-08-26 14:56:02 +0200

    work

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-source etc/setenv.sh
+source src/main/bash/setenv.sh
 
 function composeUp() {
     ./mvnw docker-compose:up
@@ -10,71 +10,29 @@ function composeDown() {
     ./mvnw docker-compose:down
 }
 
-function runDev() {
-    ./mvnw
-}
-
-function runTest() {
-    ./mvnw -B -DskipTests=false clean dependency:list install --file pom.xml
-}
-
-function runGithubTestBuild() {
-    ./mvnw -B -DskipTests clean dependency:list install --file pom.xml
-}
-
-function setupHeroku() {
-    heroku login
-    heroku ps -a simpleworklist
-}
-
-function buildLikeHerokuWithSite() {
-   ./mvnw -DskipTests=true clean dependency:list install site site:deploy
-}
-
-function buildLikeHeroku() {
-   ./mvnw -DskipTests clean dependency:list install
-}
-
-function runHerokuLocal() {
-    buildLikeHeroku
-    heroku ps -a simpleworklist
-    heroku local web
-    heroku open
-}
-
-function setup() {
-   setupHeroku
-}
-
-function build() {
-    buildLikeHerokuWithSite
-    #buildLikeHeroku
-    #runGithubTestBuild
-}
-
-function testing() {
-   runTest
-}
-
-function run() {
-    #runHerokuLocal
-    runDev
-}
-
-function release() {
-    ./mvnw -B -DskipTests release:prepare && ./mvnw -B -DskipTests release:perform && ./mvnw -B -DskipTests release:clean
-}
-
 function firstSetup() {
-    ./mvnw clean install site -DskipTests=true
+    export JAVA_OPTS=$JAVA_OPTS_RUN_DEFAULT
+    showSettings
+    ./mvnw dependency:purge-local-repository
+    ./mvnw -e -DskipTests=true clean dependency:resolve dependency:resolve-plugins dependency:sources dependency:tree
+    ./mvnw -e -DskipTests=true clean package site
+}
+
+function setupTravis() {
+    export JAVA_OPTS=$JAVA_OPTS_RUN_DEFAULT
+    showSettings
+    ./mvnw -e -DskipTests=true -B -V install -Dmaven.javadoc.skip=true
+    ./mvnw -e -DskipTests=true -B -V dependency:purge-local-repository
+    ./mvnw -e -DskipTests=true -B -V clean
+    ./mvnw -e -DskipTests=true -B -V dependency:resolve dependency:resolve-plugins dependency:sources
+    ./mvnw -e -DskipTests=true -B -V dependency:tree
+    ./mvnw -e -DskipTests=true -B -V clean package spring-boot:repackage
+    ./mvnw -e -DskipTests=true -B -V site
 }
 
 function main() {
-    ##release
-    #build
-    #run
-    firstSetup
+    # firstSetup
+    setupTravis
 }
 
 main
-
