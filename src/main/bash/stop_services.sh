c42924435607d020f1fe1de5942f31037de4d929
thomaswoehlke <thomas.woehlke@gmail.com>
2020-09-16 16:57:55 +0200

    added script for localdev env

---
+++ src/main/bash/stop_services.sh
@@ -0,0 +1,9 @@
+
+sudo systemctl stop mysql
+sudo systemctl stop apache2
+sudo systemctl stop docker
+sudo systemctl stop docker-registry
+sudo systemctl stop virtualbox
+sudo systemctl stop openvpn
+sudo systemctl stop crtmpserver
+
