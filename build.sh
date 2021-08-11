440e2c89be9a746dfd7dfa266d74d4d999ff92de
thomaswoehlke <thomas.woehlke@gmail.com>
2021-08-12 00:05:38 +0200

    work

--- build.sh
+++ build.sh
@@ -31,6 +31,18 @@ function setupTravis() {
     ./mvnw -e -DskipTests=true -B -V site site:deploy
 }
 
+function setupTravis_tmp() {
+    export JAVA_OPTS=$JAVA_OPTS_RUN_DEFAULT
+    showSettings
+    ./mvnw -e -DskipTests=true -B -V install -Dmaven.javadoc.skip=true && \
+    ./mvnw -e -DskipTests=true -B -V dependency:purge-local-repository && \
+    ./mvnw -e -DskipTests=true -B -V clean install && \
+    ./mvnw -e -DskipTests=true -B -V dependency:tree && \
+    ./mvnw -e -DskipTests=true -B -V dependency:resolve dependency:resolve-plugins dependency:sources && \
+    ./mvnw -e -DskipTests=true -B -V clean package spring-boot:repackage && \
+    ./mvnw -e -DskipTests=true -B -V site site:deploy
+}
+
 function buildJar() {
     export JAVA_OPTS=$JAVA_OPTS_RUN_DEFAULT
     showSettings
