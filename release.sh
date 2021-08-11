05497278779fddbf8690553abfa531cb2ebc8125
thomaswoehlke <thomas.woehlke@gmail.com>
2021-08-11 22:18:40 +0200

    work

--- release.sh
+++ release.sh
@@ -6,7 +6,7 @@ source ~/.bash_aliases_simpleworklist
 function releaseMe(){
     export JAVA_OPTS=$JAVA_OPTS_RUN_DEFAULT
     ./mvnw -e -DskipTests=true -B -V dependency:purge-local-repository
-    ./mvnw -e -DskipTests=true -B -V clean
+    ./mvnw -e -DskipTests=true -B -V clean install
     ./mvnw -e -DskipTests=true -B -V release:clean
     ./mvnw -e -DskipTests=true -B -V release:prepare && ./mvnw -e -DskipTests=true -B -V release:perform
 }
