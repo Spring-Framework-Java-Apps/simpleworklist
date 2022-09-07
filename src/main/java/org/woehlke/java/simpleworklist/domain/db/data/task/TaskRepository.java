2dee8081aa6eb9908cdbd881bbf6340fcbd37db8
thomaswoehlke <thomas.woehlke@gmail.com>
2022-09-07 18:20:05 +0200

    work

--- src/main/java/org/woehlke/java/simpleworklist/domain/db/data/task/TaskRepository.java
+++ src/main/java/org/woehlke/java/simpleworklist/domain/db/data/task/TaskRepository.java
@@ -8,10 +8,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.woehlke.java.simpleworklist.domain.db.data.Project;
 import org.woehlke.java.simpleworklist.domain.db.data.Task;
 import org.woehlke.java.simpleworklist.domain.meso.taskworkflow.TaskState;
 import org.woehlke.java.simpleworklist.domain.db.data.Context;
-import org.woehlke.java.simpleworklist.domain.project.Project;
 
 @Repository
 public interface TaskRepository extends JpaRepository<Task, Long> {
