f1b8120d46f36879bf09e4b9303082464dd6c17d
thomaswoehlke <thomas.woehlke@gmail.com>
2022-09-25 23:03:27 +0200

    ## 2.4.13-SNAPSHOT * fixed #380 add User Use Cases to UML Documentation * fixed #393 UML Documentation: Service Classes DB * fixed #394 UML Documentation: Service Classes Meso

---
+++ src/main/java/org/woehlke/java/simpleworklist/domain/meso/context/ContextControllerServiceImpl.java
@@ -0,0 +1,9 @@
+package org.woehlke.java.simpleworklist.domain.meso.context;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+public class ContextControllerServiceImpl implements ContextControllerService {
+}
