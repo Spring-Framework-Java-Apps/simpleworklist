347c1f6c9c96b1333e2a8a91af6a97b823cbb58d
thomaswoehlke <thomas.woehlke@gmail.com>
2022-12-26 08:48:14 +0100

    work

--- etc/dev.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-#source src/main/bash/setenv.sh
-source ~/.bash_aliases_simpleworklist
-
-./mvnw clean install spring-boot:repackage
-
-sudo cp -f target/simpleworklist.war /var/lib/tomcat9/webapps/
--- site.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-#source src/main/bash/setenv.sh
-source ~/.bash_aliases_simpleworklist
-
-./mvnw site site:deploy
